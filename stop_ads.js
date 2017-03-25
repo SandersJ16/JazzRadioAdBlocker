d8463f7ebdaa3e1c63adf94cef527704fcf21e5d
sandersj16 <justin@pointw.com>
2017-03-24 22:42:30 -0500

    Added Comments and a debug statement to help ensure it's working as expected

--- stop_ads.js
+++ stop_ads.js
@@ -1,9 +1,13 @@
 $("document").ready(function() {
+    //Remove panel that appears over player during an ad
     $("#panel-ad").remove();
+    //Remove pop up ad region
     $("#modal-region").remove();
 
+    //When the title changes check if it's an ad, if it is restart the player
     $("body").on('DOMSubtreeModified', ".title-container .title", function() {
         if ($(".title-container .title").text() == "Sponsored Message") {
+            console.debug("Skipping Ad");
             $("#ctl-play").click();
             $("#ctl-play").click();
         }
